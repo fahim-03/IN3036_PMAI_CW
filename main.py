bd016cb7996e075fa9d73993f740109da79e6770
nazifa <nazifazahan9@gmail.com>
2025-12-13 19:49:54 +0000

    dataset

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ import matplotlib.pyplot as plt
 import os
 import kaggle
 
-def main():
-  print("Hello World!")
+def download_fashion_mnist():
+    data_path = "data"
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+    
+    print("Downloading Fashion MNIST from Kaggle...")
+    # This downloads the specific dataset your team chose
+    kaggle.api.dataset_download_files(
+        'zalando-research/fashionmnist', 
+        path=data_path, 
+        unzip=True
+    )
+    print("Download complete. Files are in the /data folder.")
 
 if __name__ == "__main__":
-  main()
+    download_fashion_mnist()
 
 
-# testing git 
 
